cd55391a198b6b0e6f50ada71bd104e3a5879d8c
Matt Smith <matt.f.smith@autodesk.com>
2017-12-05 19:05:44 -0800

    Adds basic Grid

--- src/App.js
+++ src/App.js
@@ -2,26 +2,24 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Example from './components/Example/Example';
-import { Button } from 'semantic-ui-react'
+import { Button, Grid } from 'semantic-ui-react'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Example />
-        <div>
-          <Button>
-            Test Button
-          </Button>
-        </div>
-
+      <div className='App-base'>
+        <Grid columns={16} centered>
+          <Grid.Row>
+            <Grid.Column className='blue' width={8}>
+              <Example />
+            </Grid.Column>
+            <Grid.Column className='red' width={8}>
+              <Button>
+                Test Button
+              </Button>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </div>
     );
   }
